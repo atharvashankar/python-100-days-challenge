4803a5d16d6502853fda192a154f9eecdcbc0dcf
Atharva Shankar Avhad <AtharvaShankar2010@gmail.com>
2023-11-24 10:05:28 +0530

    day 1

---
+++ day1.py
@@ -0,0 +1,6 @@
+while True:
+    print("to exit enter invalid value like (a)")
+    num1 = int(input("enter first number ==>"))
+    num2 = int(input("enter second number ==>"))
+
+    print(f"your answer is {num1 + num2}")
